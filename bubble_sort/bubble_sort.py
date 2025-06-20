c53e3512728caa782127dd414fa141fda767e71c
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-06-20 13:13:17 +0300

    First commit: Bubble sort and Selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,14 +1,16 @@
 def bubble_sort(arr):
     # TODO: Implement bubble sort
-    if has_duplicates(arr):
-        print("Duplicates")
     n = len(arr)
 
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-def has_duplicates(arr):
-    return len(arr) != len(set(arr))
 
+    return arr
 
+data = [5,4,7,9,8]
+data.reverse()
+print("Reversed:", data)
+bubble_sort(data)
+print("Sorted:", data)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,10 +1,5 @@
-from bubble_sort.bubble_sort import has_duplicates
-
-
 def selection_sort(arr):
     # TODO: Implement selection sort
-    if has_duplicates(arr):
-        print("Duplicates")
     n = len(arr)
     for i in range(n):
         min_index = i
@@ -12,8 +7,12 @@ def selection_sort(arr):
             if arr[j] < arr[min_index]:
                 min_index = j
                 arr[i], arr[min_index] = arr[min_index], arr[i]
-def has_duplicates(arr):
-    return len(arr) != len(set(arr))
 
+    return arr
+data = [5,4,7,9,8]
+data.reverse()
+print("Reversed:", data)
+selection_sort(data)
+print("Sorted:", data)
 
 
