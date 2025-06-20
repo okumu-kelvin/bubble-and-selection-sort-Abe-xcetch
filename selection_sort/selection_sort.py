2e9da8fa85ed06dcb50e58c5d4fd68a5edee3dbe
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-06-20 13:24:23 +0300

    First commit: Bubble sort and Selection sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -7,6 +7,7 @@ def selection_sort(arr):
         for j in range(i+1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
+        if min_index != i:
                 arr[i], arr[min_index] = arr[min_index], arr[i]
 
     return arr
